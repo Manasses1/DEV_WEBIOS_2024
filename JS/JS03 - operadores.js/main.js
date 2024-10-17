6de113c7719c9ad60261334ee0966142c57dbe5c
Manasses1 <manasses.freitas.sp@hotmail.com>
2024-10-16 21:39:48 -0300

    Operadores logicos

---
+++ JS/JS03 - operadores.js/main.js
@@ -0,0 +1,75 @@
+'use strict'
+
+// let nota1, nota2, nota3, nota4, media;
+// nota1 = 8
+// nota2 = 5
+// nota3 = 9
+// nota4 = 7
+// media = (nota1 + nota2 + nota3 + nota4) / 4
+
+// console.log(media);
+
+
+//
+
+// let valor, num1, num2;
+// num1 = 10
+// num2 = "10"
+
+// // valor =  num1 >  num2
+// // valor = num1 >=  num2
+// // valor = num1 <   num2
+// // valor = num1 <=  num2
+// // valor = num1 ==  num2
+// // valor = num1 === num2
+// // valor = num1 !=  num2
+
+// console.log(valor);
+
+//
+
+// let num = 25;
+// let num1 = 10;
+// let nome = "Ermeson";
+// console.log(typeof num);
+// console.log(num + num1   );
+// console.log(nome.length);
+
+
+// let sol = true
+// let dinheiro = 1500
+
+// let praia = sol == true && dinheiro >= 300? "Vamos a praia" : "Não vamos a praia."
+// console.log(praia);
+
+
+// let sol = false
+// let dinheiro = 1500
+
+// let praia = sol == true || dinheiro >= 300? "Vamos a praia" : "Não vamos a praia."
+// console.log(praia);
+
+// let media = 7;
+// let falta = 8;
+// let rest = media >= 7 && falta <= 5 ? "Aprovado" : "Reprovado"
+// console.log(rest);
+
+//
+
+// let nome = prompt("Qual seu nome?");
+// alert( `Ólá ${nome}!`)
+
+// 
+
+alert("Olá mundo");
+
+let nome =  prompt("Qual seu nome?");
+
+alert(`Boas vindas ${nome}!`);
+
+let idade = prompt("Qual sua idade?");
+
+let validacao  = idade == 18? "Maior de idade" : "Menor de idade"
+console.log(validacao);
+
+alert(`${validacao}`)
