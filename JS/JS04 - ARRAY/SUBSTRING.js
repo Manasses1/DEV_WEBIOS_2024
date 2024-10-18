6d48e49cc33191a077d804a417d5a6cbc6b6ddbc
Manasses1 <manasses.freitas.sp@hotmail.com>
2024-10-17 21:17:11 -0300

    Aula 3

---
+++ JS/JS04 - ARRAY/SUBSTRING.js
@@ -0,0 +1,13 @@
+// let navegador = "Brave";
+// let novaString = navegador.substring(2)
+// let novaSubString = navegador.substring(1, 3)
+
+// console.log(novaString);
+// console.log(novaSubString);
+
+let frase = "Na moral mano!"
+
+console.log(frase);
+
+let novaF = frase.split(" ")
+console.log(novaF);
---
+++ JS/JS04 - ARRAY/array.js
@@ -0,0 +1,35 @@
+// // let grupo01 = ["Manassés", "Gabi", "Jeferson", "Sabino"]
+// // //               0          1          2             3
+// // le
+
+
+// // console.log(grupo01[2]);
+// // console.log(typeof grupo01[2]);
+
+
+
+// let teste1 = ["CBUN", "RAMON DINO", "URS", "ZANCANELLI", "BREON"]
+
+
+// // console.log(teste1);
+// // console.log(typeof teste1);
+
+// console.log(teste1[3] , teste1[4]);
+
+
+//
+
+let cupons = 'DESC110' // 10%
+// . nomr ()
+let  menor  =  cupons.toLowerCase()
+
+let  maior  =  cupons.toUpperCase()
+
+console.log(menor);
+console.log(maior);
+
+let imc = 19.8665655454
+
+let  imc1  =  imc.toFixed(2)
+
+console.log(imc1)
