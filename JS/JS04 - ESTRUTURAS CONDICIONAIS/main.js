678eb2c018dd92304fa43aba670500217275429a
Manasses1 <manasses.freitas.sp@hotmail.com>
2024-10-21 21:42:31 -0300

    Aula

---
+++ JS/JS04 - ESTRUTURAS CONDICIONAIS/main.js
@@ -0,0 +1,34 @@
+// verificar se o caio está com a blusa do  IOS
+
+
+// let caioCokBlusa =  true    
+// if(caioCokBlusa == true){
+//     console.log("Caio pode assistir a aula")
+// } else {
+//     console.log("Caio voce não vai assitir a aula.")
+// }
+
+// console.clear()
+
+// Um jogador só irá passsar se atingir 1000
+
+let pontos = 2000
+
+if (pontos >= 1001) { // sempre que for true e execultado esse bloco de codigo
+    console.log("Passou para a fase 2")
+}
+
+console.clear()
+
+
+// resto da divisão do numero por 2 for 0 então é PAR
+// resto da divisão do numero por 2 for 1 então é IMPAR
+
+let imparPar = 188
+
+if (imparPar % 2  == 0) {
+    console.log("Par")
+    
+} else {
+    console.log("Impar")
+}
