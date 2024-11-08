67fdde6c522575702a02a27f678c396f33be93a0
Manasses1 <manasses.freitas.sp@hotmail.com>
2024-11-07 21:42:22 -0300

    Aula

--- JS/JS09-MetadoArray/main.js
+++ JS/JS09-MetadoArray/main.js
@@ -2,16 +2,25 @@
 let frutas = ["Banana", "Abacate", "Uva", "Melancia", "Abacaxi", "Tomate", "Morango", "Acerola"] // coloque o nome de 8 frutas
 
 
-frutas.forEach(Percorrer)
-
-
-// Primeiro parametro conteudo, segundo indice. REGRA!!!
-function Percorrer(valor, indice) {
+frutas.forEach((valor, indice) => {
     console.log(frutas[valor]); // retorna valor(conteudo) da posição
     console.log(indice); // Retorna numero da posição
-}
+})
 
-// for(i = 0; i <= frutas.length ; i++){
-//     console.log(frutas[i]); // retorna valor(conteudo) da posição
-//     console.log(i); // Retorna numero da posição
+
+
+
+
+// frutas.forEach(Percorrer)
+
+
+// // Primeiro parametro conteudo, segundo indice. REGRA!!!
+// function Percorrer(valor, indice) {
+//     console.log(frutas[valor]); // retorna valor(conteudo) da posição
+//     console.log(indice); // Retorna numero da posição
 // }
+
+// // for(i = 0; i <= frutas.length ; i++){
+// //     console.log(frutas[i]); // retorna valor(conteudo) da posição
+// //     console.log(i); // Retorna numero da posição
+// // }
