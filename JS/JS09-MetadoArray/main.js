fa840df5167db489226bfce465aaf585e170c8b1
Manasses1 <manasses.freitas.sp@hotmail.com>
2024-11-08 20:51:28 -0300

    Aula

--- JS/JS09-MetadoArray/main.js
+++ JS/JS09-MetadoArray/main.js
@@ -2,9 +2,23 @@
 let frutas = ["Banana", "Abacate", "Uva", "Melancia", "Abacaxi", "Tomate", "Morango", "Acerola"] // coloque o nome de 8 frutas
 
 
-frutas.forEach((valor, indice) => {
-    console.log(frutas[valor]); // retorna valor(conteudo) da posição
-    console.log(indice); // Retorna numero da posição
+
+
+
+
+let infoAlunos = [
+    {"Ana": 25},
+    {"João": 35},
+    {"Pedro": 50}
+]
+
+
+
+//forEach ele faz a função do for
+infoAlunos.forEach((valor, indice) => {
+    if (indice == 1){
+        console.log(valor)
+    }
 })
 
 
@@ -24,3 +38,64 @@ frutas.forEach((valor, indice) => {
 // //     console.log(frutas[i]); // retorna valor(conteudo) da posição
 // //     console.log(i); // Retorna numero da posição
 // // }
+
+console.clear()
+
+
+// Math.currently ou Math.max (Retorna o Maior) / Math.min (Retorna o Menor) / Math.sqrt (Raiz quadrada) 
+let numeros = [4, 16, 64, 32, 9, 1000000]
+
+// Trazer um novo array atualizado com a raiz quadrada do arrayde cada numero do array atual 
+// let numerosDois = numeros.map(Math.sqrt)
+let numerosDois = numeros.map(Math.sqrt)
+let maiorNumero = Math.max.apply(Math, numeros)
+let menorNumero = Math.min.apply(Math, numeros)
+
+console.log(maiorNumero + " " + menorNumero)
+// console.log(numeros)
+// console.log(numerosDois)
+
+
+let filtraNumero = numeros.filter((valor) => {
+return valor >= 16;
+})
+
+console.log(numeros)
+console.log(filtraNumero)
+
+console.clear()
+
+
+let tarefa = [
+    {tarefa: "Dormir",
+     isFeita: true
+    },
+
+    {tarefa: "limpar",
+     isFeita: false
+    },
+
+    {tarefa: "torcer",
+     isFeita: true
+    }
+]
+
+//tertorne apenas as tarefas que estão como true
+// find
+
+
+
+let retornaTrue = tarefa.filter((item) => {
+    return item.isFeita === true
+})
+
+console.log(retornaTrue)
+
+
+
+
+
+
+
+
+
