3b53f5d4c3c881f36c057cb7de0bfc7901be57d6
Manasses1 <manasses.freitas.sp@hotmail.com>
2024-11-14 21:34:18 -0300

    Aula

---
+++ JS/JS13 - DOM 3/main.js
@@ -0,0 +1,17 @@
+let botao = document.querySelector('button')
+
+let olhaNumero = true
+if(olhaNumero == true){
+    botao.style.color = "blue"
+    botao.style.backgroundColor = "green"
+    botao.style.botderRadius = "5px"
+    botao.innerHTML = "Cadastrar"
+}else{
+    botao.style.color = "red"
+}
+
+let paragrafo = document.querySelector('#paragrafo')
+paragrafo.style.backgroundImage = 'linear-gradient(red, yellow'
+paragrafo.style.color = "white"
+
+
